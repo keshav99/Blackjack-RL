e0c450b769556cedfd0fadf16114657ef8a12e3b
Keshav Moorthy <kesh.howzat@gmail.com>
2025-04-22 15:46:41 +0530

    Initial basic blackjack game and cartpole game

--- Blackjack Problem in RL/train_mc_agent.py
+++ Blackjack Problem in RL/train_mc_agent.py
@@ -5,7 +5,7 @@ import numpy as np
 
 env = gym.make('Blackjack-v1', natural=True, sab=True) # sab true for single agent blackjack
 agent = MCAgent(eps=0.3, gamma=0.98)
-num_episodes = 200000
+num_episodes = 20000
 wins = []
 total_wins = 0
 
@@ -13,19 +13,22 @@ print("Training the agent...")
 for i in range(1, num_episodes + 1):
     state = env.reset()[0] # reset the environment and get the initial state
     done = False
-
+    
     while not done:
         action = agent.choose_action(state) # agent chooses action
         next_state, reward, done, _, _ = env.step(action)  # then the environment takes the action
         agent.episode.append((state, action, reward)) # the episode is appended with the state, action and reward
         state = next_state
     
+    
+
+    
     if reward > 0:
         total_wins += 1
     if i % 1000 == 0:
         wins.append(total_wins / i)
     agent.update_q() # update the Q values and policy after each episode
-    if i % 5000 == 0:
+    if i % 1000 == 0:
         print(f"Episode {i}: Total Wins: {total_wins}")
 
 plt.plot(range(0, len(wins) * 1000, 1000), wins)
